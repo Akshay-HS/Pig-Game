ad11a05e08f0622e588db2d7accdf12e8933b19d
Alvin Ragi <alvin.ragi@experionglobal.local>
2023-11-04 12:25:14 +0530

    feat changing the current value when die is rolled

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 'use strict';
+let sum = 20;
+// setCurrentValue function, sum variable, currentPlayer variable;
+
+function setCurrentValue(currentPlayer){
+    sum = sum+20;
+    document.getElementById(`current--${currentPlayer}`).textContent = `${sum}`;
+}
