d25e967c10930f9c034c4c1f1f0ab7d80aaf90ea
Sruthi Sathyan <sruthi.sathyan@experionglobal.local>
2023-11-04 15:42:39 +0530

    :feat:Added reset function

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-'use strict';
-function Restart()
-{
-    // setting score and current score of Player 1 to initial state
+// this is common
+let sum = 0;
+let currentPlayer = 0;
+//This code is done by shiyas
+
+function randomNumber(){
+    
+    let currentRoll=(Math.floor(Math.random() *6)+1);
+    document.getElementById("dice-img").src=`dice-${currentRoll}.png`
+
+    if (currentRoll == 1){
+        if(currentPlayer ==0){
+            currentPlayer = 1;
+        }
+        else{
+            currentPlayer = 0;
+        }
+    }
+    setCurrentValue(currentPlayer, currentRoll);
+}
+
+//This code is by Alvin
+
+// setCurrentValue function, sum variable, currentPlayer variable;
+function setCurrentValue(currentPlayer, currentRoll){
+    sum = sum+currentRoll;
+    document.getElementById(`current--${currentPlayer}`).textContent = `${sum}`;
+}
+
+ // setting score and current score of Player 1 to initial state
+ function restart()
+ {
     const Player1Score=document.getElementById("score--0");
     Player1Score.textContent="0";
     const Player1CurrentScore=document.getElementById("current--0");
@@ -11,4 +39,4 @@ function Restart()
     Player2Score.textContent="0";
     const Player2CurrentScore=document.getElementById("current--1");
     Player2CurrentScore.textContent="0";
-}
+ }
