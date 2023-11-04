fd7b55814414c5dbdac2d453b5ce30060c18ea19
Abhijith A Nair <abhijith.nair@experionglobal.local>
2023-11-04 12:42:29 +0530

    feat:- added hold button functions updated corrections made

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-'use strict';
+function HoldCurrentStored(index) {
+  let currentvalue = document.getElementById(`current--${index}`).textContent;
+  let storedvalue = document.getElementById(`score--${index}`).textContent;
+  let currentvalueint = parseInt(currentvalue);
+  let storedvalueint = parseInt(storedvalue);
+  storedvalueint = storedvalueint + currentvalueint;
+  console.log(storedvalueint);
+  currentvalueint = 0;
+  document.getElementById(`current--${index}`).textContent =
+    currentvalueint.toString();
+  document.getElementById(`score--${index}`).textContent =
+    storedvalueint.toString();
+}
