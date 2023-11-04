1df698e66f228d822b10e36af28772970ae92adf
Abhijith A Nair <abhijith.nair@experionglobal.local>
2023-11-04 16:38:14 +0530

    feat:- added hold button functions to the exisiting work

--- script.js
+++ script.js
@@ -1,4 +1,51 @@
-function HoldCurrentStored(index) {
+// this is common
+let sum = 0;
+let currentPlayer = 0;
+//This code is done by shiyas
+
+function randomNumber() {
+  let currentRoll = Math.floor(Math.random() * 6) + 1;
+  document.getElementById('dice-img').src = `dice-${currentRoll}.png`;
+
+  if (currentRoll == 1) {
+    sum = 0;
+    if (currentPlayer == 0) {
+      sum = 0;
+      currentPlayer = 1;
+    } else {
+      sum = 0;
+      currentPlayer = 0;
+    }
+  }
+  setCurrentValue(currentPlayer, currentRoll);
+}
+
+//This code is by Alvin
+
+// setCurrentValue function, sum variable, currentPlayer variable;
+function setCurrentValue(currentPlayer, currentRoll) {
+  sum = sum + currentRoll;
+  document.getElementById(`current--${currentPlayer}`).textContent = `${sum}`;
+}
+function restart() {
+  const Player1Score = document.getElementById('score--0');
+  Player1Score.textContent = '0';
+  const Player1CurrentScore = document.getElementById('current--0');
+  Player1CurrentScore.textContent = '0';
+  //setting score and current score of Player 2 to initial state
+  const Player2Score = document.getElementById('score--1');
+  Player2Score.textContent = '0';
+  const Player2CurrentScore = document.getElementById('current--1');
+  Player2CurrentScore.textContent = '0';
+}
+function HoldCurrentStored() {
+  sum = 0;
+  index = currentPlayer;
+  if (currentPlayer == 0) {
+    currentPlayer = 1;
+  } else {
+    currentPlayer = 0;
+  }
   let currentvalue = document.getElementById(`current--${index}`).textContent;
   let storedvalue = document.getElementById(`score--${index}`).textContent;
   let currentvalueint = parseInt(currentvalue);
