47b06089e802370229a4e77cba05e3fbe5ac6c5a
Abhijith A Nair <83201816+abhijithanair@users.noreply.github.com>
2023-11-07 08:54:20 +0530

    feat-added progress bar

--- script.js
+++ script.js
@@ -1,35 +1,31 @@
-// this is common
 let sum = 0;
 let currentPlayer = 0;
-//This code is done by shiyas
-
 
 function randomNumber() {
   let currentRoll = Math.floor(Math.random() * 6) + 1;
   document.getElementById('dice-img').src = `dice-${currentRoll}.png`;
-
   if (currentRoll == 1) {
-    sum = 0;
+    //sum = 0;
     if (currentPlayer == 0) {
+      document.getElementById('current--0').textContent = 0;
       sum = 0;
       currentPlayer = 1;
     } else {
+      document.getElementById('current--1').textContent = 0;
       sum = 0;
       currentPlayer = 0;
     }
+    setCurrentValue(currentPlayer, 0);
+  } else {
+    setCurrentValue(currentPlayer, currentRoll);
   }
-  setCurrentValue(currentPlayer, currentRoll);
-
 }
-
-//This code is by Alvin
-
-// setCurrentValue function, sum variable, currentPlayer variable;
 
 function setCurrentValue(currentPlayer, currentRoll) {
   sum = sum + currentRoll;
   document.getElementById(`current--${currentPlayer}`).textContent = `${sum}`;
 }
+
 function restart() {
   const Player1Score = document.getElementById('score--0');
   Player1Score.textContent = '0';
@@ -41,6 +37,7 @@ function restart() {
   const Player2CurrentScore = document.getElementById('current--1');
   Player2CurrentScore.textContent = '0';
 }
+
 function HoldCurrentStored() {
   sum = 0;
   index = currentPlayer;
@@ -60,23 +57,11 @@ function HoldCurrentStored() {
     currentvalueint.toString();
   document.getElementById(`score--${index}`).textContent =
     storedvalueint.toString();
+  const percentage = (storedvalueint / 100) * 100;
+  updateProgressBar(index, storedvalueint);
 }
-function setCurrentValue(currentPlayer, currentRoll){
-    sum = sum+currentRoll;
-    document.getElementById(`current--${currentPlayer}`).textContent = `${sum}`;
+function updateProgressBar(player, value) {
+  const progressBar = document.getElementById(`progress-bar-${player}`);
+  value = Math.max(0, Math.min(value, 100));
+  progressBar.style.height = `${value}%`; // Use height to adjust the vertical progress
 }
-
- // setting score and current score of Player 1 to initial state
- function restart()
- {
-    const Player1Score=document.getElementById("score--0");
-    Player1Score.textContent="0";
-    const Player1CurrentScore=document.getElementById("current--0");
-    Player1CurrentScore.textContent="0"; 
-    //setting score and current score of Player 2 to initial state
-    const Player2Score=document.getElementById("score--1");
-    Player2Score.textContent="0";
-    const Player2CurrentScore=document.getElementById("current--1");
-    Player2CurrentScore.textContent="0";
- }
-
